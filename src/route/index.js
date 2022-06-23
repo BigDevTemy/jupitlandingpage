c96c991ddc5fe1bc72b4894da93ec5824a215b20
wabloan <wabloan@wabloan.com>
2022-06-23 02:08:28 +0100

    ok

--- src/route/index.js
+++ src/route/index.js
@@ -39,132 +39,132 @@ const route = [
     //     component:Contact,
 
     // },
-    {
-        path:'/user/changepassword/:codeid/:userid',
-        title:'Reset Password | Jupit',
-        isAuthenticated:false,
-        component:ChangePassword,
+    // {
+    //     path:'/user/changepassword/:codeid/:userid',
+    //     title:'Reset Password | Jupit',
+    //     isAuthenticated:false,
+    //     component:ChangePassword,
 
-    },
-    {
-        path:'/client/signin',
-        title:'Client SignIn | Jupit',
-        isAuthenticated:false,
-        component:Login,
+    // },
+    // {
+    //     path:'/client/signin',
+    //     title:'Client SignIn | Jupit',
+    //     isAuthenticated:false,
+    //     component:Login,
 
-    },
-    {
-        path:'/client/signup',
-        title:'Client SignUp | Jupit',
-        isAuthenticated:false,
-        component:Register,
+    // },
+    // {
+    //     path:'/client/signup',
+    //     title:'Client SignUp | Jupit',
+    //     isAuthenticated:false,
+    //     component:Register,
 
-    },
-    {
-        path:'/client/kyc',
-        title:'Client KYC | Jupit',
-        isAuthenticated:true,
-        component:Dashboard,
+    // },
+    // {
+    //     path:'/client/kyc',
+    //     title:'Client KYC | Jupit',
+    //     isAuthenticated:true,
+    //     component:Dashboard,
 
-    },
-    {
-        path:'/client/transactions-history',
-        title:'Client Transactions | Jupit',
-        isAuthenticated:true,
-        component:Dashboard,
+    // },
+    // {
+    //     path:'/client/transactions-history',
+    //     title:'Client Transactions | Jupit',
+    //     isAuthenticated:true,
+    //     component:Dashboard,
 
-    },
-    {
-        path:'/client/wallet',
-        title:'Client Wallet | Jupit',
-        isAuthenticated:true,
-        component:Dashboard,
+    // },
+    // {
+    //     path:'/client/wallet',
+    //     title:'Client Wallet | Jupit',
+    //     isAuthenticated:true,
+    //     component:Dashboard,
 
-    },
-    {
-        path:'/client/settings',
-        title:'Client Settings | Jupit',
-        isAuthenticated:true,
-        component:Dashboard,
+    // },
+    // {
+    //     path:'/client/settings',
+    //     title:'Client Settings | Jupit',
+    //     isAuthenticated:true,
+    //     component:Dashboard,
 
-    },
-    {
-        path:'/client/notification',
-        title:'Client Notification | Jupit',
-        isAuthenticated:true,
-        component:Dashboard,
+    // },
+    // {
+    //     path:'/client/notification',
+    //     title:'Client Notification | Jupit',
+    //     isAuthenticated:true,
+    //     component:Dashboard,
 
-    },
-    {
-        path:'/client/logout',
-        title:'Client Logout | Jupit',
-        isAuthenticated:true,
-        component:Dashboard,
+    // },
+    // {
+    //     path:'/client/logout',
+    //     title:'Client Logout | Jupit',
+    //     isAuthenticated:true,
+    //     component:Dashboard,
 
-    },
-    {
-        path:'/client/sendbtc',
-        title:'Client Dashboard | Jupit',
-        isAuthenticated:true,
-        component:Dashboard,
+    // },
+    // {
+    //     path:'/client/sendbtc',
+    //     title:'Client Dashboard | Jupit',
+    //     isAuthenticated:true,
+    //     component:Dashboard,
 
-    },
-    {
-        path:'/client/sendusdt',
-        title:'Client Dashboard | Jupit',
-        isAuthenticated:true,
-        component:Dashboard,
+    // },
+    // {
+    //     path:'/client/sendusdt',
+    //     title:'Client Dashboard | Jupit',
+    //     isAuthenticated:true,
+    //     component:Dashboard,
 
-    },
-    {
-        path:'/client/tradegiftcard',
-        title:'Client Dashboard | Jupit',
-        isAuthenticated:true,
-        component:Dashboard,
+    // },
+    // {
+    //     path:'/client/tradegiftcard',
+    //     title:'Client Dashboard | Jupit',
+    //     isAuthenticated:true,
+    //     component:Dashboard,
 
-    },
-    {
-        path:'/client/buybtc',
-        title:'Client Dashboard | Jupit',
-        isAuthenticated:true,
-        component:Dashboard,
+    // },
+    // {
+    //     path:'/client/buybtc',
+    //     title:'Client Dashboard | Jupit',
+    //     isAuthenticated:true,
+    //     component:Dashboard,
 
-    },
-    {
-        path:'/client/buyusdt',
-        title:'Client Dashboard | Jupit',
-        isAuthenticated:true,
-        component:Dashboard,
+    // },
+    // {
+    //     path:'/client/buyusdt',
+    //     title:'Client Dashboard | Jupit',
+    //     isAuthenticated:true,
+    //     component:Dashboard,
 
-    },
-    {
-        path:'/client/sellbtc',
-        title:'Client Dashboard | Jupit',
-        isAuthenticated:true,
-        component:Dashboard,
+    // },
+    // {
+    //     path:'/client/sellbtc',
+    //     title:'Client Dashboard | Jupit',
+    //     isAuthenticated:true,
+    //     component:Dashboard,
 
-    },
-    {
-        path:'/client/withdrawal',
-        title:'Client Dashboard | Jupit',
-        isAuthenticated:true,
-        component:Dashboard,
+    // },
+    // {
+    //     path:'/client/withdrawal',
+    //     title:'Client Dashboard | Jupit',
+    //     isAuthenticated:true,
+    //     component:Dashboard,
 
-    },
-    {
-        path:'/client/sellusdt',
-        title:'Client Dashboard | Jupit',
-        isAuthenticated:true,
-        component:Dashboard,
+    // },
+    // {
+    //     path:'/client/sellusdt',
+    //     title:'Client Dashboard | Jupit',
+    //     isAuthenticated:true,
+    //     component:Dashboard,
 
-    },
-    {
-        path:'/client',
-        title:'Client Dashboard | Jupit',
-        isAuthenticated:true,
-        component:Dashboard,
+    // },
+    // {
+    //     path:'/client',
+    //     title:'Client Dashboard | Jupit',
+    //     isAuthenticated:true,
+    //     component:Dashboard,
 
-    },
+    // },
     
     
     
