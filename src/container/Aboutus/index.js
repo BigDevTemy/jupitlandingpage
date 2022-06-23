b6a78cbf049e0ba88618a3a7635e88a8cd37342a
wabloan <wabloan@wabloan.com>
2022-06-23 01:56:00 +0100

    ok

--- src/container/Aboutus/index.js
+++ src/container/Aboutus/index.js
@@ -1,14 +1,14 @@
 import React from "react";
 import Header from '../../component/header';
-// import HeroAboutus from '../../layout/AboutusLayout/HeroAbout'
-// import Section1 from '../../layout/AboutusLayout/Section1'
-// import Section2 from '../../layout/AboutusLayout/Section2'
+import HeroAboutus from '../../layout/AboutusLayout/HeroAbout'
+import Section1 from '../../layout/AboutusLayout/Section1'
+import Section2 from '../../layout/AboutusLayout/Section2'
 
-// import Section3 from '../../layout/AboutusLayout/Section3'
-// import Section4 from '../../layout/AboutusLayout/Section4'
-// import Section5 from '../../layout/AboutusLayout/Section5'
-// import Team from '../../layout/HomeLayout/Ourteam'
-// import Footer from '../../component/xfooter'
+import Section3 from '../../layout/AboutusLayout/Section3'
+import Section4 from '../../layout/AboutusLayout/Section4'
+import Section5 from '../../layout/AboutusLayout/Section5'
+import Team from '../../layout/HomeLayout/Ourteam'
+import Footer from '../../component/xfooter'
 import { useState,useEffect } from "react";
 const Index=()=>{
     const [scroll,setscroll] = useState(0)
@@ -17,15 +17,15 @@ const Index=()=>{
       }, [])
     return(
         <div>
-            {/* <Header getscroll={setscroll} />
+            <Header getscroll={setscroll} />
             <HeroAboutus/>
             <Section5/>
             <Section3/>
             <Section4/>
             <Team/>
-            <Footer/> */}
-            {/* <Section1/>
-            <Section2/> */}
+            <Footer/> 
+             <Section1/>
+            <Section2/>
            
             
         </div>
